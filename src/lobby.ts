453e50bec78bdf11b7e2f73eb0948e3f9f0f38a8
srpk <saranpongk@gmail.com>
2024-04-27 17:20:33 +0700

    Initial commit

---
+++ src/lobby.ts
@@ -0,0 +1,52 @@
+import { Server } from "./server";
+import { User } from "./user";
+
+export class Lobby
+{
+  private users: Map<number, User>;
+
+  public constructor(_server: Server)
+  {
+    this.users = new Map<number, User>();
+  }
+
+  public addUser(user: User)
+  {
+    this.broadcast((playerId, player) => player.sendEnterUser(user));
+    user.sendEnterUser(user);
+    this.users.set(user.getUserId(), user);
+  }
+
+  public removeUser(user: User)
+  {
+    this.users.delete(user.getUserId())
+  }
+
+  public getUser(userId: number)
+  {
+    return this.users.get(userId);
+  }
+
+  public chat(sender: User, message: string)
+  {
+    this.broadcast((playerId, player) => {
+      player.sendChat(sender.getUserId(), sender.getUserData().displayName, message)
+    });
+  }
+
+  private broadcast(handler: (playerId: number, player: User) => void)
+  {
+    for (const [id, player] of this.users)
+    {
+      if (player !== undefined)
+      {
+        handler(id, player);
+      }
+    }
+  }
+
+  public close()
+  {
+    this.users.clear();
+  }
+}
